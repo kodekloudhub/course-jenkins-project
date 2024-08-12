cc2eb1a848c5cc57ae3bbfdcf15691c1d5b0871c
sanjeev <sanjeev@example.com>
2024-08-12 00:04:26 -0600

    asdfj

--- lambda-app/hello_world/app.py
+++ lambda-app/hello_world/app.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world v6",
+            "message": "hello world v7",
             # "location": ip.text.replace("\n", "")
         }),
     }
