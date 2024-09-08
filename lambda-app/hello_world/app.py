0389f11b9acb91bf96e09ddbcdefb37ab89a3523
sanjeev <sanjeev@example.com>
2024-09-07 19:00:30 -0600

    v2

--- lambda-app/hello_world/app.py
+++ lambda-app/hello_world/app.py
@@ -9,7 +9,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello world v1",
+            "message": "hello world v2",
             # "location": ip.text.replace("\n", "")
         }),
     }
